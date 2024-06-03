bb85b9e4617800a8fe52f71178f56a3d286986a2
bellison137 <benrellison@gmail.com>
2024-06-02 23:00:50 -0700

    another build fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,48 +1,5 @@
-/* eslint-disable no-restricted-globals */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// src/service-worker.js
+const { precacheAndRoute } = workbox.precaching;
 
-const CACHE_NAME = 'my-app-cache';
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json', // Include the manifest file
-          // Add other assets as needed
-          '/static/js/bundle.js', // Example path to your main JavaScript bundle
-          '/static/css/main.css', // Example path to your main CSS file
-          // Add paths to other assets included in the manifest
-        ]);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+precacheAndRoute(self.__WB_MANIFEST);
