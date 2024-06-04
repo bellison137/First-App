e3250ec8c00508770220c4cdac144fcf94becabe
bellison137 <benrellison@gmail.com>
2024-06-03 18:44:21 -0700

    manual build fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { registerServiceWorker } from './serviceworkerregistration'; // Import registerServiceWorker
+import { registerServiceWorker } from './serviceWorkerRegistration'; // Import registerServiceWorker
 
 ReactDOM.render(
   <React.StrictMode>
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-restricted-globals */
+
 // Import workbox directly
 import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
-
